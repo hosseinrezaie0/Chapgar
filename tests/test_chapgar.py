79df3c151da207c2262bea172ef908141ce0c915
hosseinrezaie0 <hossein.reazaie81@gmail.com>
2026-08-12 12:55:35 +0330

    test Chapgar

--- tests/test_chapgar.py
+++ tests/test_chapgar.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from core.generator import create_pdf
 
 # A comprehensive stress test containing all common Persian RTL issues
